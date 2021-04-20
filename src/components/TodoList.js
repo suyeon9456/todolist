495173c174c95a4fd79f0d98b06147271142f329
suyeon <suyeon9456@naver.com>
2021-04-20 19:06:14 +0900

    TodoList - 프로젝트 생성 UI 준비 -2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import TodoItem from './TodoItem'
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -10,7 +11,12 @@ const TodoListBlock = styled.div`
 
 function TodoList () {
   return (
-    <TodoListBlock>TOdoList</TodoListBlock>
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기" done />
+      <TodoItem text="컴포넌트 스타일링 하기" done />
+      <TodoItem text="Context 만들기" done={false} />
+      <TodoItem text="기능 구현하기" done={false} />
+    </TodoListBlock>
   )
 }
 
